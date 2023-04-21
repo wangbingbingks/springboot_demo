2ebda14ebc24a6857e6a01ba22295ab9ddc3b5a1
wangbingbing <wangbingbing@dycjr.com>
2023-04-21 23:35:39 +0800

    init

---
+++ src/main/java/com/example/springboot_demo/SpringbootDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RequestMapping
+@RestController
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+    @GetMapping("/111")
+    public String getqq() {
+        return "hello wangbing";
+    }
+}
